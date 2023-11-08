c98bff53337af9ba7beb03cb1ae8bb0f91a5c2f2
Prasad Kutarekar <prasad.kutarekar@gmail.com>
2023-11-08 23:07:29 +0530

    Updates- 08-11-2023

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,13 +7,13 @@
                     <div class="text-main">
                         <v-text class="text-h3 logo-header">Dr Dipti's Clinic</v-text>
                         <p class="text-h5">Multispeciality Homeopathy Clinic</p>
-                        <p>We offer tailored healthcare, including infertility treatments, homeopathy for natural healing, skin care, pediatric care,
+                        <p class="text-body-1">We offer tailored healthcare, including infertility treatments, homeopathy for natural healing, skin care, pediatric care,
                             women's health, and chronic solutions, blending tradition with modern expertise for holistic care.</p>
                         <v-row style="padding-bottom: 10px;">
-                            <v-col> <v-img :width="1000" :height="250" aspect-ratio="16/9" cover src="@/assets/pink_med.jpg"></v-img></v-col>
+                            <v-col> <v-img  cover src="@/assets/pink_med.jpg"></v-img></v-col>
                             <!-- <v-col> <v-img aspect-ratio="16/9" cover src="@/assets/green_med.jpg"></v-img></v-col> -->
                         </v-row>
-                        <a href="https://forms.gle/8pMjwKXcz1tN1Gvr5"><v-btn size="x-large" color="site">Book your appointment now</v-btn></a>
+                        <a href="https://forms.gle/8pMjwKXcz1tN1Gvr5"><v-btn width="100%" size="x-large" color="site">Book your appointment now</v-btn></a>
                         <!-- <p class="text-h6"> Your journey to better health starts here, where we prioritize your well-being above all else.</p> -->
                     </div>
                 </v-col>
@@ -27,16 +27,16 @@
                         <v-card height="100%">
                             <div class="d-flex flex-no-wrap justify-space-between">
                                 <div>
-                                    <v-card-title class="text-h5 text-card1">
+                                    <v-card-title class="text-h5">
                                         Homeopathy
                                     </v-card-title>
-                                    <v-card-text class="text-card1">
+                                    <v-card-text class="text-body-1">
                                         <div>Opt for homeopathy for chronic cases and experience holistic healing. It delves into the underlying causes,
                                             offering safe and natural solutions that promote long-term well-being, making it an excellent choice for
                                             lasting relief.</div>
                                     </v-card-text>
 
-                                    <v-card-actions class="text-card1">
+                                    <v-card-actions class="text-card">
                                         <v-btn href="https://forms.gle/8pMjwKXcz1tN1Gvr5" class="ms-2" variant="outlined" size="small">
                                             START NOW
                                         </v-btn>
@@ -57,15 +57,15 @@
                         <v-card height="100%">
                             <div class="d-flex flex-no-wrap justify-space-between">
                                 <div>
-                                    <v-card-title class="text-h5 text-card2">
+                                    <v-card-title class="text-h5">
                                         Gynecology
                                     </v-card-title>
-                                    <v-card-text class="text-card2">
+                                    <v-card-text class="text-body-1">
                                         <div>Choose homeopathy for gynecology to embrace a holistic and non-invasive approach. It addresses the root
                                             causes of female health concerns, harnessing the body's natural healing ability for personalized, safe, and
                                             effective well-being.</div>
                                     </v-card-text>
-                                    <v-card-actions class="text-card2">
+                                    <v-card-actions class="text-card">
                                         <v-btn href="https://forms.gle/8pMjwKXcz1tN1Gvr5" class="ms-2" variant="outlined" size="small">
                                             START NOW
                                         </v-btn>
--- src/views/PricingView.vue
+++ src/views/PricingView.vue
@@ -1,15 +1,98 @@
 <template>
-  <div>
-    <h2>Hello from pricing</h2>
-  </div>
+  <v-container class="about-container">
+    <v-row>
+            <v-col class="text-center mt-4 " cols="12">
+                <!-- <v-img max-width="100" class="mx-auto" src="@/assets/logo2.png" /> -->
+                <div class="text-main">
+                    <v-text class="text-h3 logo-header">Dr Dipti's Clinic</v-text>
+                </div>
+            </v-col>
+        </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <template v-slot:title>
+            First Consultaion
+          </template>
+
+          <template v-slot:subtitle>
+            Including medicines and basic check-up
+          </template>
+
+          <template v-slot:text>
+            ₹ 1000
+          </template>
+        </v-card>
+        <v-card>
+          <template v-slot:title>
+            Repeat Consultaion
+          </template>
+
+          <template v-slot:subtitle>
+            Including medicines and basic check-up
+          </template>
+
+          <template v-slot:text>
+            ₹ 800
+          </template>
+        </v-card>
+        <v-card>
+          <template v-slot:title>
+            Skin Consultaion
+          </template>
+
+          <template v-slot:subtitle>
+            Including medicines and basic check-up
+          </template>
+
+          <template v-slot:text>
+            ₹ 1000
+          </template>
+        </v-card>
+        <v-card>
+          <template v-slot:title>
+            Laser Clensing
+          </template>
+
+          <template v-slot:subtitle>
+            Including medicines and basic check-up
+          </template>
+
+          <template v-slot:text>
+            ₹ 1000
+          </template>
+        </v-card>
+        <v-card>
+          <template v-slot:title>
+            Tatoo Removal
+          </template>
+
+          <template v-slot:subtitle>
+            Including medicines and basic check-up
+          </template>
+
+          <template v-slot:text>
+            * As per size
+          </template>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <a href="https://forms.gle/8pMjwKXcz1tN1Gvr5"><v-btn width="100%" size="x-large" color="site">Book your appointment now</v-btn></a>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-img src="@/assets/blue_med.jpg"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-export default {
 
-}
+<script setup>
+const variants = ['elevated', 'flat', 'tonal', 'outlined']
 </script>
 
-<style>
-
-</style>
+<style></style>
